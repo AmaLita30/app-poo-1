60c5d78311c0fff74d5539d7de4f17f9eee5b513
AmaLita30 <ortiz.chavez.amalia@gmail.com>
2025-02-20 20:20:56 -0500

    Aplicando Polimorfismo a la clase alumno y docente

--- src/model/Alumno.java
+++ src/model/Alumno.java
@@ -1,5 +1,11 @@
 package model;
 
 public class Alumno extends Persona {
-
+    @Override
+    public boolean registrarAsitencia(String codIdentificador, String fechaAdistencia, String HoraAsistencia, Boolean valorAsistencia) {
+        System.out.println("Aplicar la Lógica " +
+                "para la Asistencia del " +
+                "Alumno");
+        return true;
+    }
 }
--- src/model/Docente.java
+++ src/model/Docente.java
@@ -1,4 +1,11 @@
 package model;
 
 public class Docente extends Persona{
+    @Override
+    public boolean registrarAsitencia(String codIdentificador, String fechaAdistencia, String HoraAsistencia, Boolean valorAsistencia) {
+        System.out.println("Aplicar Lógica " +
+                "para la Asistencia del " +
+                "Docente");
+        return true;
+    }
 }
