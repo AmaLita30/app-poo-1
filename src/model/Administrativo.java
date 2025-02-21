381a6e8a616f73123897d5f4136933b22ba0b536
AmaLita30 <ortiz.chavez.amalia@gmail.com>
2025-02-20 20:16:27 -0500

    Aplicando polimorfismo a la clase administrativo

--- src/model/Administrativo.java
+++ src/model/Administrativo.java
@@ -1,4 +1,13 @@
 package model;
 
+import javax.sound.midi.Soundbank;
+
 public class Administrativo extends Persona {
+    @Override
+    public boolean registrarAsitencia(String codIdentificador, String fechaAdistencia, String HoraAsistencia, Boolean valorAsistencia) {
+        System.out.println("Aplicar Lógica " +
+                "para la asistencia del " +
+                "administrativo ");
+        return true;
+    }
 }
